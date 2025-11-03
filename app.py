822bb65d4be80f800feeee4757df8c9eb54e192e
Michael Posylkin <Michael@Michaels-MacBook-Pro-6.local>
2025-11-03 14:30:44 +0000

    added ABOUT/CONTACT pages

--- app.py
+++ app.py
@@ -139,17 +139,17 @@ class ProjectForm(FlaskForm):
     submit = SubmitField('Upload Project')
 
 # Routes
-# @app.route('/')
-# def index():
-#     return render_template('base.html')
-
-
 @app.route("/")
 def home():
     return render_template("home.html")
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
-
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 
 @app.route("/register", methods=["GET", "POST"])
